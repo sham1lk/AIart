9b1791f87a68417abaa26b90844b4bec8a3dca5d
sham1lk <khastiev@gmail.com>
2020-04-27 21:55:45 +0300

    done

--- main.js
+++ main.js
@@ -11,14 +11,21 @@ let fs = require('fs');
 let PNG = require('pngjs').PNG;
 
 
-//setings 
-const population_size = 5;
-const cube_size = 16;
-const gen_size = 8;
-var data = fs.readFileSync('in.png'); //input must be 512x512 png
+//settings 
+const population_size   = 16;
+const square_size 	    = 32;
+const chromosome_size   = 20;
+const finish_percentage = 99.5;
+const input             = 'in.png'
+const output            = 'out.png'
 
 
-var png  = PNG.sync.read(data);
+
+
+var data = fs.readFileSync(input);
+
+
+var png  = PNG.sync.read(data);			//reading image
 var empty = PNG.sync.read(data);
 var best = PNG.sync.read(data);
 for (var i = 0; i < png.data.length; i++) {
@@ -34,68 +41,57 @@ let data1 = [];
 
 
 
-let cube_num = (512/cube_size)*(512/cube_size);
-let populations = [];
-genalg();
+let square_num = (512/square_size)*(512/square_size);
+let populations = [];	
 
-
-
-function genalg(){ 
-
-	start();
-
-}
-
+start();
 
 
 function start(){
 
-	for(let i = 0; i<cube_num;i++) {
-		generate();
+	let percentage = 0;
+	for(let i = 0; i<square_num;i++) {		
+		generate();									//generating first population
 	}
 
-	while(1){
-	data1 = [];
-	for(let i = 0; i<cube_num;i++) {
-		update(i);
-	}
-	best.data = [];
-	for(let z = 0;z<cube_num;z+=512/cube_size)
-		for(let k = 0;k<cube_size;k++)
-			for(let i = 0;i<512/cube_size;i++)
-				for(let j = 0; j<cube_size*4;j++){
-					best.data.push(data1[i+z][j+k*cube_size*4]);
-					
-				}
-	
+	while(percentage<finish_percentage){
+		data1 = [];
+		for(let i = 0; i<square_num;i++) {
+			update(i);									//generating new population
+		}
+		best.data = [];
+		for(let z = 0;z<square_num;z+=512/square_size)
+			for(let k = 0;k<square_size;k++)							// Combine all parts into one image
+				for(let i = 0;i<512/square_size;i++)
+					for(let j = 0; j<square_size*4;j++){
+						best.data.push(data1[i+z][j+k*square_size*4]);
+						
+					}
+		
 
-	create_png(best);
-	console.log(fitnessglobal(best.data));
+		create_png(best);
+		percentage =fitnessglobal(best.data);
+		console.log(percentage);
 	}
 	return 0;
 }
 
 
-function generate(	){
+function generate(){						//generating first population
 
 	let population = [];
 	for(let j = 0; j<population_size;j++){
 		
 		let circles =[];
 		
-		for (var i = 0; i < gen_size; i++) {
-			let circle = new Сircle(Math.floor(cube_size*Math.random()),Math.floor(cube_size*Math.random()),Math.floor(cube_size*Math.random()/2),
+		for (var i = 0; i < chromosome_size; i++) {
+			let circle = new Сircle(Math.floor(square_size*Math.random()),Math.floor(square_size*Math.random()),Math.floor(square_size*Math.random()/2),
 									[Math.floor(256*Math.random()),Math.floor(256*Math.random()),Math.floor(256*Math.random())]);
 			circles.push(circle);
 		}
 		population.push(circles);
 	}
 	populations.push(population);
-	// let k = 1;
-	// while(k>0)
-	// {	
-	// 	
-	// }
 }
 
 
@@ -110,7 +106,7 @@ function update(i){
 
 
 
-function new_population(p,f){
+function new_population(p,f){  
 	let min1 = 999999999;
 	let min1_i = 0;
 	let min2 = 999999999;
@@ -141,12 +137,12 @@ function new_population(p,f){
 function crossover(np){
 	let child1 = [];
 	let child2 = [];
-	let cp = Math.floor((gen_size-1)*Math.random());
+	let cp = Math.floor((chromosome_size-1)*Math.random());
 	for (var i = 0; i < cp; i++) {
 		child1.push(np[0][i]);
 		child2.push(np[1][i]);
 	}
-	for (var i = cp; i < gen_size; i++) {
+	for (var i = cp; i < chromosome_size; i++) {
 		child1.push(np[1][i]);
 		child2.push(np[0][i]);
 	}
@@ -164,12 +160,12 @@ function mutation(np){
 
 function mutate(gen){
 	let new_g = [];
-	for (var i = 0; i < gen_size; i++) {
+	for (var i = 0; i < chromosome_size; i++) {
 		new_g.push(gen[i]);
 	}
-	let circle = new Сircle(Math.floor(cube_size*Math.random()),Math.floor(cube_size*Math.random()),Math.floor(cube_size/2*Math.random()),
+	let circle = new Сircle(Math.floor(square_size*Math.random()),Math.floor(square_size*Math.random()),Math.floor(square_size/2*Math.random()),
 								[Math.floor(256*Math.random()),Math.floor(256*Math.random()),Math.floor(256*Math.random())]);
-	new_g[Math.floor((gen_size-1)*Math.random())] = circle;
+	new_g[Math.floor((chromosome_size)*Math.random())] = circle;
 
 	return new_g;
 }
@@ -178,12 +174,12 @@ function mutate(gen){
 function fitness(a,i){
 	let b = 0;
 
-		for (var j =  0; j < cube_size; j++) {
+		for (var j =  0; j < square_size; j++) {
 				
-			for(let k = 0; k< cube_size; k++){
-				let g = (Math.abs(a[4*k+j*4*cube_size]-png.data[Math.floor(i/(512/cube_size))*cube_size*2048+j*2048+(i%(512/cube_size))*cube_size*4+k*4]));
-				g+=(Math.abs(a[4*k+j*4*cube_size+1]-png.data[Math.floor(i/(512/cube_size))*cube_size*2048+j*2048+(i%(512/cube_size))*cube_size*4+k*4+1]));
-				g+=(Math.abs(a[4*k+j*4*cube_size+2]-png.data[Math.floor(i/(512/cube_size))*cube_size*2048+j*2048+(i%(512/cube_size))*cube_size*4+k*4+2]));
+			for(let k = 0; k< square_size; k++){
+				let g = (Math.abs(a[4*k+j*4*square_size]-png.data[Math.floor(i/(512/square_size))*square_size*2048+j*2048+(i%(512/square_size))*square_size*4+k*4]));
+				g+=(Math.abs(a[4*k+j*4*square_size+1]-png.data[Math.floor(i/(512/square_size))*square_size*2048+j*2048+(i%(512/square_size))*square_size*4+k*4+1]));
+				g+=(Math.abs(a[4*k+j*4*square_size+2]-png.data[Math.floor(i/(512/square_size))*square_size*2048+j*2048+(i%(512/square_size))*square_size*4+k*4+2]));
 				b+=g;
 	
 			}
@@ -196,7 +192,7 @@ function fitness(a,i){
 
 function draw(d){
 	let data = [];
-	for (var i = 0; i < cube_size*cube_size*4; i++) {
+	for (var i = 0; i < square_size*square_size*4; i++) {
 		data.push(255);
 	}
 	
@@ -204,12 +200,12 @@ function draw(d){
 	for(let i =0;i<d.length;i++){
 		let c = d[i];
 		
-		for (var j = 0; j < cube_size; j++) {
-			for(let k = 0; k<cube_size;k++){
+		for (var j = 0; j < square_size; j++) {
+			for(let k = 0; k<square_size;k++){
 				if(((k - c.x) * (k - c.x) + (j - c.y) * (j - c.y)) < c.r * c.r){
-					data[k*4+j*cube_size*4]=Math.floor((data[k*4+j*cube_size*4]+c.color[0])/2);
-					data[(k*4+j*cube_size*4)+1]=Math.floor((data[(k*4+j*cube_size*4)+1]+c.color[1])/2);
-					data[(k*4+j*cube_size*4)+2]=Math.floor((data[(k*4+j*cube_size*4)+2]+c.color[2])/2);
+					data[k*4+j*square_size*4]=Math.floor((data[k*4+j*square_size*4]+c.color[0])/2);
+					data[(k*4+j*square_size*4)+1]=Math.floor((data[(k*4+j*square_size*4)+1]+c.color[1])/2);
+					data[(k*4+j*square_size*4)+2]=Math.floor((data[(k*4+j*square_size*4)+2]+c.color[2])/2);
 				}
 			}
 		}
@@ -220,7 +216,7 @@ function draw(d){
 
 function create_png(image){
 	let buffer = PNG.sync.write(image);
-		fs.writeFileSync('out.png', buffer); // output file name
+		fs.writeFileSync(output, buffer);
 }
 
 
